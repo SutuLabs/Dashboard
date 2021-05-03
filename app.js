a74863325a533bca69667f5f55e4ab45342beb66
LiangShuang <liangshuang@sutu.tech>
2021-05-03 11:25:58 +0800

    beautify machine display

--- app.js
+++ app.js
@@ -232,7 +232,10 @@ var app = new Vue({
             };
             console.log(this.diskMap);
         },
-
+        humanize(size) {
+            var i = Math.floor( Math.log(size) / Math.log(1024) );
+            return ( size / Math.pow(1024, i) ).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
+        },
     },
     computed: {},
 })
