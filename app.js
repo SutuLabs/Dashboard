ae428143d026fc62ee23c16c9fdf0a7c24d7728b
Liu Ya <liuya@sutu.tech>
2021-05-11 11:32:46 +0800

    check if farm exists

--- app.js
+++ app.js
@@ -311,15 +311,10 @@ var app = new Vue({
             }
         },
         calculate() {
+            if(!this.farm) return; 
             const unitPlotSize = 101.4; 
             var nPlot = (this.nPlot==""||this.nPlot<0)? 1:parseInt(this.nPlot); 
-            var rawTotalNetSpace; //EiB
-            try {
-                rawTotalNetSpace = parseFloat(this.farm.node.space);
-            } catch(err) {
-                console.log(err); 
-                return ;
-            }; 
+            var rawTotalNetSpace = parseFloat(this.farm.node.space); //EiB
             var totalNetSpace = 0; 
             totalNetSpace = rawTotalNetSpace*1024; 
             var ownedNetSpace = (nPlot*unitPlotSize)/(rawTotalNetSpace*Math.pow(1024,3))*100; 
