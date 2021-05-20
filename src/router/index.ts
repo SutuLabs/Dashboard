d313924a74158d700ddfb2675632e2a935eab18d
Liu Ya <liuya@sutu.tech>
2021-05-20 09:56:10 +0800

    add webpack chunk name

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,27 +16,27 @@ const routes: Array<RouteConfig> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Explorer.vue')
+    component: () => import(/* webpackChunkName: "explorer" */ '../views/Explorer.vue')
   },
   {
     path: '/calculator',
     name: 'Calculator',
-    component: () => import('../views/Calculator.vue')
+    component: () => import(/* webpackChunkName: "calculator" */ '../views/Calculator.vue')
   },
   {
     path: '/monitor',
     name: 'Monitor',
-    component: () => import('../views/Monitor.vue')
+    component: () => import(/* webpackChunkName: "monitor" */ '../views/Monitor.vue')
   },
   {
     path: '/about',
     name: 'About',
-    component: () => import('../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
     path: '/plottingPerformance',
     name: 'Plotting Performance',
-    component: () => import('../views/PlottingPerformance.vue')
+    component: () => import(/* webpackChunkName: "plottingPerformance" */ '../views/PlottingPerformance.vue')
   },
 ]
 
