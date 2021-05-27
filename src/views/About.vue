0b2d143f862717b2ae1472374c1a40022bdd883b
Liu Ya <liuya@sutu.tech>
2021-05-27 17:10:58 +0800

    open official site in new tab

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 ﻿<template>
   <div class="about">
-    <p>chiabee由上海素图科技有限公司开发，了解更多请访问素图科技官网：<a href="https://www.sutu.tech/">https://www.sutu.tech/</a></p>
+    <p>chiabee由上海素图科技有限公司开发，了解更多请访问素图科技官网：<a href="https://www.sutu.tech/"  target="_blank">https://www.sutu.tech/</a></p>
   </div>
 </template>
