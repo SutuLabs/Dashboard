cbd4a86d1f3e00498c3101666d6bfdd19908ba76
Liu Ya <liuya@sutu.tech>
2021-06-04 16:17:08 +0800

    combine login and about tab

--- src/App.vue
+++ src/App.vue
@@ -28,15 +28,19 @@
           </b-navbar-item>
           <b-dropdown :triggers="['hover']" position="is-bottom-left" aria-role="menu">
             <template #trigger>
-              <b-navbar-item >
+              <b-navbar-item>
                 <span v-if="!login">个人中心</span>
                 <span v-else>{{username}}</span>
                 <b-icon icon="menu-down"></b-icon>
               </b-navbar-item>
             </template>
             <b-dropdown-item aria-role="menuitem" v-if="login" @click="quit">退出登录</b-dropdown-item>
-            <b-navbar-item aria-role="menuitem" v-else tag="router-link" :to="{path:'/login'}">登录</b-navbar-item>
-            <b-navbar-item aria-role="menuitem" tag="router-link" :to="{path:'/about'}">关于我们</b-navbar-item>
+            <b-dropdown-item aria-role="menuitem" v-else>
+              <b-navbar-item tag="router-link" :to="{path:'/login'}" class="p-0">登录</b-navbar-item>
+            </b-dropdown-item>
+            <b-dropdown-item aria-role="menuitem">
+              <b-navbar-item tag="router-link" :to="{path:'/about'}" class="p-0">关于我们</b-navbar-item>
+            </b-dropdown-item>
           </b-dropdown>
           
         </template>
@@ -81,28 +85,26 @@
             </div>
             <div class="is-size-7">矿场监控</div>
           </b-navbar-item>
-          <b-navbar-item class="navbarButton" tag="router-link" :to="{path:'/about'}">
-            <div>
-              <b-icon icon="information"></b-icon>
-            </div>
-            <div class="is-size-7">关于</div>
-          </b-navbar-item>
-          <b-navbar-item v-if="!login" class="navbarButton" tag="router-link" :to="{path:'/login'}">
-            <div>
-              <b-icon icon="account"></b-icon>
-            </div>
-            <div class="is-size-7">登录</div>
-          </b-navbar-item>
-          <b-dropdown v-else :triggers="['click']" position="is-top-left" aria-role="menu">
+          <b-dropdown :triggers="['click']" position="is-top-left" aria-role="menu">
             <template #trigger>
               <b-navbar-item class="navbarButton">
                 <div>
                   <b-icon icon="account"></b-icon>
                 </div>
-                <div class="is-size-7">{{ username }}</div>
+                <div class="is-size-7">个人中心</div>
               </b-navbar-item>
             </template>
-            <b-dropdown-item aria-role="menuitem" @click="quit">退出登录</b-dropdown-item>
+            <b-dropdown-item v-if="login" aria-role="menuitem" @click="quit">
+              亲爱的<span class="has-text-success">{{ username }}</span>，您已成功登录。
+              <hr class="dropdown-divider">
+              退出登录
+            </b-dropdown-item>
+            <b-dropdown-item v-else aria-role="menuitem">
+              <b-navbar-item tag="router-link" :to="{path:'/login'}" class="p-0">登录</b-navbar-item>
+            </b-dropdown-item>
+            <b-dropdown-item area-role="menuitem">
+              <b-navbar-item tag="router-link" :to="{path:'/about'}" class="p-0">关于我们</b-navbar-item>
+            </b-dropdown-item>
           </b-dropdown>
         </template>
       </b-navbar>
@@ -133,7 +135,7 @@
   @import 'bulmaswatch/darkly/_overrides.scss';
 
   .navbarButton {
-    width: 20vw;
+    width: 25vw;
     display: flex;
     flex-direction: column;
   }
