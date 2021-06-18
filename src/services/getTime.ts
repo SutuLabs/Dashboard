bb5aeb6ee57dbfb8ebe96809a21722e167ee32ff
zhangwenda <zhangwenda@sutu.tech>
2021-06-18 02:57:38 +0000

    Updated getTime.ts

--- src/services/getTime.ts
+++ src/services/getTime.ts
@@ -11,28 +11,28 @@ export default {
       const hour = Math.floor(time / 60);
       const min = Math.floor(time - hour * 60);
       if (hour < 1) {
-        if (time < 1) return "Less than 1 minute";
-        else return "About " + time.toFixed(0).toString() + " minutes";
+        if (time < 1) return "少于一分钟";
+        else return "大约 " + time.toFixed(0).toString() + " 分钟";
       } else {
 
-        if (min < 1) return hour.toString() + " hours";
-        else return hour.toString() + " hours " + min.toString() + " minutes";
+        if (min < 1) return hour.toString() + " 小时";
+        else return hour.toString() + " 小时 " + min.toString() + " 分钟";
       }
     } else if (day < 31) {
-      return day.toFixed(0).toString() + " days";
+      return day.toFixed(0).toString() + " 天";
     } else {
       const month = Math.floor(day / 30);
       const temp_day = Math.floor(day - month * 30);
       if (month < 12) {
-        return month.toString() + " months " + temp_day.toString() + " days";
+        return month.toString() + " 个月 " + temp_day.toString() + " 天";
       } else {
         const year = Math.floor(day / 365);
-        if (year < 1) return "Almost 1 year"
+        if (year < 1) return "近一年"
         else {
           const temp_month = Math.floor((day - year * 365) / 30);
-          if (temp_month < 1) return year.toString() + " years";
+          if (temp_month < 1) return year.toString() + " 年";
           else {
-            return year.toString() + " years " + temp_month.toString() + " months";
+            return year.toString() + " 年 " + temp_month.toString() + " 个月";
           }
         }
       }
