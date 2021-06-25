abd1ef587ec6972d0873fce91484ad4ec4e95920
zhangwenda <zhangwenda@sutu.tech>
2021-06-25 09:22:38 +0000

    modify legacyPlotter

--- src/views/Monitor.vue
+++ src/views/Monitor.vue
@@ -323,7 +323,7 @@ export default class monitor extends Vue {
   plotPlan: any = null;
   username = localStorage.getItem('username');
   checkable = false;
-  legacyPlotter = false;
+  legacyPlotter = this.$refs.machine.legacyPlotter
 
   $refs!: {
     harvester: machineTableDetailed,
